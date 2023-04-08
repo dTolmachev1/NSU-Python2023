d6f57fe5cc7b61fce3b372180969e0e5a1f78403
dtolmachev1 <d.tolmachev1@g.nsu.ru>
2023-04-08 09:28:51 +0700

    Problems-1. Task2 main added

--- problems-1/d-tolmachev/problem2.py
+++ problems-1/d-tolmachev/problem2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from typing import Iterable
+
 # replaces all out of range sequence elements to it's boundaries
-def replace_out_of_range(seq: list[int], a: int, b: int) -> list[int]:
-    return list(map(lambda x: check_boundaries(x, a, b), seq))    # O(n)
+def replace_out_of_range(seq: Iterable[int], a: int, b: int) -> tuple[int]:
+    return tuple(map(lambda x: check_boundaries(x, a, b), seq))    # O(n)
 
 # Returns input iff it belongs to the specified range, or one of it's boundaries otherwise
 def check_boundaries(x: int, l: int, u: int) -> int:
@@ -11,3 +13,7 @@ def check_boundaries(x: int, l: int, u: int) -> int:
     if x > u:
         return u
     return x
+
+# Used as entrypoint
+if __name__ == '__main__':
+    print(replace_out_of_range([0, 1, 2, 3, 4], 1, 3))
