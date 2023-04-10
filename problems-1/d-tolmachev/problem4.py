2a86ecf692d6c6454c96b1ed044946f1bcad7ed0
dtolmachev1 <d.tolmachev1@g.nsu.ru>
2023-04-10 10:04:24 +0700

    Problems-1. Task4 fixed

--- problems-1/d-tolmachev/problem4.py
+++ problems-1/d-tolmachev/problem4.py
@@ -2,13 +2,16 @@
 
 quantities: list[str] = ['no', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']    # Numbers as strings
 
+# Pluralize word if necessary
+def bottle(i: int) -> str:
+    return 'bottle' + ('s' if i != 1 else '')
+
 # Used as entrypoint
 if __name__ == '__main__':
     for i in range(10, 0, -1):
+        print(f'{quantities[i].capitalize()} green {bottle(i)} hanging on the wall,\n' * 2, end = '')
         if i == 1:
-            print(f'{quantities[i].capitalize()} green bottle hanging on the wall,\n' * 2, end = '')
             print('If that one green bottle should accidentally fall,')
         else:
-            print(f'{quantities[i].capitalize()} green bottles hanging on the wall,\n' * 2, end = '')
             print('And if one green bottle should accidentally fall,')
-        print(f'There’ll be {quantities[i - 1]} green bottles hanging on the wall.')
+        print(f'There’ll be {quantities[i - 1]} green {bottle(i - 1)} hanging on the wall.')
